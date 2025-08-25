4ba4b6c8561a97bcc217a8782b77a4b435427e2a
worknshubh <worknshubh@gmail.com>
2025-08-25 05:33:42 +0530

    refactor: update signinUser cookie settings to allow cross-site usage

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -51,7 +51,7 @@ const signinUser = async (req, res) => {
       const token = jsonwebtoken.sign({ id: userCheck._id }, JWT_KEY);
       res.cookie("token", token, {
         httpOnly: true,
-        sameSite: "lax",
+        sameSite: "none",
         secure: true,
         maxAge: 24 * 60 * 60 * 1000,
       });
